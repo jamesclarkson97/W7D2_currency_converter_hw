a58ec4bb57c806634683f59a40235e4cb2a6d02b
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-07-28 19:18:12 +0100

    FINALY figured out how to make the converter work

--- src/app.js
+++ src/app.js
@@ -4,17 +4,30 @@ document.addEventListener('DOMContentLoaded', ()=> {
     new Vue({
         el: '#app',
         data: {
-            exchangeRates: null
+            exchangeRates: null,
+            selectedCurrency: null,
+            euros: 1,
+            exchangedRate: 0
         },
         mounted() {
             this.fetchData();
+        },
+        computed: {
+            // setExchangedRate: function() {
+            //     this.exchangedRate === this.selectedCurrency
+            // }
         },
         methods: {
             fetchData() {
                 fetch("https://api.exchangeratesapi.io/latest")
                 .then(response => response.json())
                 .then(data => this.exchangeRates = data);
-            }
+            },
+            exchangeToForeign() {
+                console.log(this.selectedCurrency);
+                this.exchangedRate = (this.euros * this.selectedCurrency);
+                return this.exchangedRate
+            },
         }
     })
 })
