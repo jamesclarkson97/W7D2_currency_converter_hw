e83368824ca07260919cf7f73caedefa1d1b38ab
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-07-28 18:07:41 +0100

    Listed currency and rate onsite

--- src/app.js
+++ src/app.js
@@ -3,6 +3,18 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', ()=> {
     new Vue({
         el: '#app',
-        data: {}
+        data: {
+            exchangeRates: null
+        },
+        mounted() {
+            this.fetchData();
+        },
+        methods: {
+            fetchData() {
+                fetch("https://api.exchangeratesapi.io/latest")
+                .then(response => response.json())
+                .then(data => this.exchangeRates = data);
+            }
+        }
     })
 })
